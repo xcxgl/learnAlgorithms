1ac58efb484bc8c3bed0d252179fd2dfc9955766
xcxgl <1244513267@qq.comm>
2016-07-31 23:55:48 +0800

    First commit. Add insertion sort algorithms.

---
+++ src/foundations/InsertionSort.java
@@ -0,0 +1,26 @@
+package foundations;
+
+public class InsertionSort {
+	static void sort(int array[]){
+		for(int i = 1; i < array.length; i++){
+			//key为本轮要插入的数字
+			int key = array[i];
+			int j = i - 1;
+			//大于key的数向后移
+			while(j >= 0 && array[j] > key){
+				array[j+1] = array[j];
+				j--;
+			}
+			//插入key
+			array[j+1] = key;
+		}
+		System.out.print("Result:");
+		for(int i = 0; i < array.length; i++)
+			System.out.print(" " + array[i]);
+	}
+	
+	public static void main(String[] args){
+		int array[] = {5,2,4,6,1,3};
+		sort(array);
+	}
+}
