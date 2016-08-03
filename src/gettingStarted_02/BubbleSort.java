e2fd6c70adf22e284d39c1eb4203fe2726f700e6
xcxgl <1244513267@qq.com>
2016-08-03 18:12:32 +0800

    Add CheckSums.java and BubbleSort.java

---
+++ src/gettingStarted_02/BubbleSort.java
@@ -0,0 +1,32 @@
+package gettingStarted_02;
+
+/**
+ * First Part  Foundations
+ * Chapter 2  Getting Started
+ * Page 40 Problems 2.2 BUBBLESORT
+ */
+
+public class BubbleSort {
+	static void bubbleSort(int[] A){
+		//i计数冒泡执行的趟数，也是本轮冒泡要放的位置
+		for(int i = 0; i < A.length - 1; i++){
+			for(int j = A.length -1; j > i; j--){
+				if(A[j] < A[j-1]){
+					//交换
+					int temp = A[j];
+					A[j] = A[j-1];
+					A[j-1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args){
+		int[] A = {3, 1, 2, 5, 4};
+		bubbleSort(A);
+		System.out.print("Result:");
+		for(int i = 0; i < A.length; i++){
+			System.out.print(" " + A[i]);
+		}
+	}
+}
---
+++ src/gettingStarted_02/CheckSums.java
@@ -0,0 +1,38 @@
+package gettingStarted_02;
+
+/**
+ * First Part  Foundations
+ * Chapter 2  Getting Started
+ * Page 39 Exercises 2.3-7 
+ * Describe a n*lgn time algorithm that, 
+ * given a set S of n integers and another integer x, 
+ * determines whether or not there exist 
+ * two elements in S whose sum is exactly x.
+ * Solution: 
+ * 1.Merge sort
+ * 2.Search from both start and end. 
+ */
+
+public class CheckSums {
+	static boolean checkSums(int[] S, int x){
+		MergeSort.mergeSort(S, 0, S.length - 1);
+		int p = 0;
+		int q = S.length - 1;
+		//从首尾两个方向同时向中间查找
+		while(p < q){
+			if(p + q == x) 
+				return true;
+			if(p + q > x) 
+				q--;
+			else
+				p++;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args){
+		int[] S = {2, 1, 4, 3, 5, 7, 6, 8};
+		int x = 11;
+		System.out.println(checkSums(S, x));
+	}
+}
